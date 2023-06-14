fffb881cf54dace69adb04dc2407f34c75ecd3c1
Martijn-Kalkman <71371406+Martijn-Kalkman@users.noreply.github.com>
2023-06-14 14:21:31 +0200

    Update Level1.js

--- src/js/Level1.js
+++ src/js/Level1.js
@@ -2,30 +2,25 @@ import * as ex from 'excalibur';
 import { Resources, ResourceLoader } from './resources.js';
 
 import { Platform } from './Platform';
-// import { Player } from './player';
+import { Background } from './Background';
 
 export class Level1 extends ex.Scene {
+  DataClass;
 
-    DataClass
+  constructor(DataClass) {
+    super({});
+    this.DataClass = DataClass;
+  }
 
-    constructor(DataClass) {
-        super({})
-        this.DataClass = DataClass
-    }
+  onInitialize(engine) {
+    const backgroundImage = Resources.hofbg1.toSprite(); 
+    const background = new Background(0, 80, 200, 20, backgroundImage);
+    this.add(background);
 
-    onInitialize(Engine) {
-        // const fish = new Actor();
-        // fish.graphics.use(Resources.Fish.toSprite());
-        // fish.pos = new Vector(400, 300);
-        // fish.vel = new Vector(-10, 0);
-        // this.add(fish);
-    
-        const platform1 = new Platform(0, 0, 2000, 20, ex.Color.Green);
-        this.add(platform1);
-    
-        // const player = new Player(100, 100, 50, 50);
-        // game.add(player);
-    }
+    const platform1 = new Platform(0, 580, 2000, 20, ex.Color.Green);
+    this.add(platform1);
 
-
+    // const player = new Player(100, 100, 50, 50);
+    // game.add(player);
+  }
 }
