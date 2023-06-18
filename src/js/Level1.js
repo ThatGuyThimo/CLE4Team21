ccedd524d56afec3b5022f1f5d252e46785a7982
Martijn-Kalkman <71371406+Martijn-Kalkman@users.noreply.github.com>
2023-06-18 09:30:15 +0200

    Update Level1.js

--- src/js/Level1.js
+++ src/js/Level1.js
@@ -3,24 +3,47 @@ import { Resources, ResourceLoader } from './resources.js';
 
 import { Platform } from './Platform';
 import { Background } from './Background';
+import { Coin } from './Coin';
+import { Player } from './player';
 
 export class Level1 extends ex.Scene {
-  DataClass;
-
   constructor(DataClass) {
-    super({});
+    super();
     this.DataClass = DataClass;
   }
 
   onInitialize(engine) {
-    const backgroundImage = Resources.hofbg1.toSprite(); 
-    const background = new Background(0, 80, 200, 20, backgroundImage);
+    const backgroundImage = Resources.hofbg1.toSprite();
+    const background = new Background(0, 80, 300, 20, backgroundImage);
     this.add(background);
 
     const platform1 = new Platform(0, 580, 2000, 20, ex.Color.Green);
     this.add(platform1);
 
-    // const player = new Player(100, 100, 50, 50);
-    // game.add(player);
+    const scoreCounter = {
+      score: 0,
+      incrementScore: function () {
+        this.score++;
+        console.log('Score:', this.score);
+      },
+    };
+
+    const player = new Player();
+    this.add(player);
+
+    const coinImage = Resources.sign.toSprite();
+    const coin = new Coin(100, 200, 32, 32, coinImage);
+    this.add(coin);
+
+    // Check for collision at every update
+    this.update = function (engine, delta) {
+      if (player.collides(coin)) {
+        console.log('Collided with coin');
+        scoreCounter.incrementScore();
+        coin.kill();
+      }
+
+      ex.Scene.prototype.update.call(this, engine, delta);
+    };
   }
 }
