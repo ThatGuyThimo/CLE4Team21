eb8b559f1981ed2cf3316a15b4fb884f0bfcceaa
Martijn-Kalkman <71371406+Martijn-Kalkman@users.noreply.github.com>
2023-06-18 09:29:30 +0200

    Update Coin.js

--- src/js/Coin.js
+++ src/js/Coin.js
@@ -1,1 +1,26 @@
+import * as ex from 'excalibur';
+import { Player } from './player';
 
+
+export class Coin extends ex.Actor {
+    constructor(x, y, width, height, image, scoreCounter) {
+      super({
+        pos: new ex.Vector(x, y),
+        width: width,
+        height: height,
+        anchor: ex.Vector.Zero,
+      });
+  
+      const sprite = new ex.Sprite(image);
+      this.graphics.use(sprite);
+  
+      this.scoreCounter = scoreCounter;
+  
+      this.on('precollision', (event) => {
+        if (event.other instanceof Player) {
+          this.scoreCounter.incrementScore();
+          this.kill();
+        }
+      });
+    }
+  }
