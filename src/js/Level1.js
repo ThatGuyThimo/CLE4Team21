4aa08fc04da36f9c6a22b0bfbfedc77e892a291d
thimo <thimodehaan@gmail.com>
2023-06-19 14:56:59 +0200

    added score system and added score label

--- src/js/Level1.js
+++ src/js/Level1.js
@@ -5,10 +5,13 @@ import { Platform } from './Platform';
 import { BackgroundClass } from './Background.js';
 import { Coin } from './Coin';
 import { Player } from './Player.js';
+import { Scorelabel } from './Scorelabel.js';
 
 export class Level1 extends ex.Scene {
 
   player
+  scorelabel
+  DataClass
 
   constructor(DataClass) {
     super();
@@ -16,25 +19,32 @@ export class Level1 extends ex.Scene {
   }
 
   onInitialize(engine) {
-    this.initaializeActors()
-    this.initaializeCamera()
-    this.initaializeBackground()
+    this.initaializeActors();
+    this.initaializeCamera();
+    this.initaializeBackground();
+  }
+
+  onPreUpdate() {
+    this.scorelabel.updateText(`Score: ${this.DataClass.getScore()}`)
+    console.log(this.DataClass.getScore())
   }
 
   initaializeActors() {
     this.player = new Player(new ex.Vector(100, 520), this.DataClass, false, 1, new ex.Vector(1.5,1.5), 200);
     const platform1 = new Platform(0, 585, 90000, 20);
-    const coin = new Coin(new ex.Vector(300, 500), 64, 64, new ex.Vector(1, 1), this.DataClass)
+    const coin = new Coin(new ex.Vector(900, 500), 64, 64, new ex.Vector(1, 1), this.DataClass);
+    this.scorelabel = new Scorelabel();
 
     this.add(this.player);
     this.add(platform1);
-    this.add(coin)
+    this.add(this.scorelabel);
+    this.add(coin);
 
   }
   initaializeBackground() {
-    const foreground = new BackgroundClass(-2, -100, 50, new ex.Vector(1,1), 1,  Resources.background[0])
-    const middleground = new BackgroundClass(-3, -125, -10, new ex.Vector(1,1), 0.8,  Resources.background[1])
-    const background = new BackgroundClass(-4, -250, -150, new ex.Vector(1,1.2), 0.6,  Resources.background[2])
+    const foreground = new BackgroundClass(-2, -100, 50, new ex.Vector(1,1), 1,  Resources.background[0]);
+    const middleground = new BackgroundClass(-3, -125, -10, new ex.Vector(1,1), 0.8,  Resources.background[1]);
+    const background = new BackgroundClass(-4, -250, -150, new ex.Vector(1,1.2), 0.6,  Resources.background[2]);
 
     this.add(foreground);
     this.add(middleground);
@@ -47,13 +57,13 @@ export class Level1 extends ex.Scene {
       -2000,
       40000,
       610
-    )
+    );
 
-    this.camera.strategy.lockToActor(this.player)
-    this.camera.strategy.limitCameraBounds(boundingBox)
+    this.camera.strategy.lockToActor(this.player);
+    this.camera.strategy.limitCameraBounds(boundingBox);
   }
 
   reset() {
-    this.player.pos = new ex.Vector(100, 520)
+    this.player.pos = new ex.Vector(100, 520);
   }
 }
